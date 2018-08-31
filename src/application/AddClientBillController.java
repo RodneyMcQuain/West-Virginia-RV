13071a2ece47992a3b61bc0c8c700ffd5925a287
Rodney McQuain <rodneymcquain95@gmail.com>
2018-08-31 11:59:02 -0400

    Added number validation for check number, debit transaction number, and amount

--- src/application/AddClientBillController.java
+++ src/application/AddClientBillController.java
@@ -72,6 +72,19 @@ public class AddClientBillController {
 			GUIUtils.emptyTextAlert();
 			return;
 		}
+
+		if (cbPaymentType.getValue().equals(PaymentUtils.CHECK_PAYMENT_STRING) && !GUIUtils.isNumber(tfCheckNumber)) {
+			GUIUtils.errorAlert("Not Number", "Please enter a number into the check number textfield.");
+			return;
+		} else if (cbPaymentType.getValue().equals(PaymentUtils.DEBIT_PAYMENT_STRING) && !GUIUtils.isNumber(tfDebitTransactionNumber)) {
+			GUIUtils.errorAlert("Not Number", "Please enter a number into the debit transaction number textfield.");
+			return;
+		}
+		
+		if (!GUIUtils.isNumber(tfAmount)) {
+			GUIUtils.errorAlert("Not Number", "Please enter a number into the amount textfield.");
+			return;
+		}
 		
 		String paymentType = cbPaymentType.getValue();
 		int paymentTypeID = PaymentUtils.getPaymentTypeIDByString(paymentType);
