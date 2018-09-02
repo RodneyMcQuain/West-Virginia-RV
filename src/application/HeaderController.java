d875e55b0af0ba0f9d7025a7b477d0471bcb5a1f
Rodney McQuain <rodneymcquain95@gmail.com>
2018-09-02 16:20:09 -0400

    Changed the back button algorithm
    Now the addition of an fxml string and controller is handled by the History class, so there is a more succint solution for a back button.  Instead of adding every FXMLStringAndController the addToHistory method now only adds non-duplicates thus simplifying the method for loading the fxml.

--- src/application/HeaderController.java
+++ src/application/HeaderController.java
@@ -1,44 +1,20 @@
 package application;
 
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
+import model.History;
 import model.utility.FXMLReferences;
 
 public class HeaderController {
 	@FXML
 	public void onClick_btBack() {
-		int lastIndexNum = Main.history.size() - 1;
-		
-		lastIndexNum = validateList(lastIndexNum);
-				
-		FXMLStringAndController fxmlStringAndController = Main.history.get(lastIndexNum);
+		History.removeMostRecent();
+		ArrayList<FXMLStringAndController> history = History.getHistory();
+		int lastIndexNum = history.size() - 1;
+						
+		FXMLStringAndController fxmlStringAndController = history.get(lastIndexNum);
 		fxmlStringAndController.goToFXML();
-	}
-	
-	/**
-	 * If last element and 2nd to last element are equal to each other get rid of both
-	 * to avoid endless looping. If not remove just the last element like normal.
-	 * 
-	 * @param lastIndexNum - Current last index number.
-	 * @return lastIndexNum - Updated last index number, reflecting new list size.
-	*/
-	private int validateList(int lastIndexNum) {
-		if (lastIndexNum >= 1) {
-			if (!(Main.history.get(lastIndexNum).equals(Main.history.get(lastIndexNum - 1)))) {
-				lastIndexNum = removeLastElementAndDecrement(lastIndexNum);
-			} else {
-				lastIndexNum = removeLastElementAndDecrement(lastIndexNum);
-				lastIndexNum = removeLastElementAndDecrement(lastIndexNum);
-			}
-		}
-		
-		return lastIndexNum;
-	}
-	
-	private int removeLastElementAndDecrement(int lastIndexNum) {
-		Main.history.remove(lastIndexNum);
-		lastIndexNum--;
-		
-		return lastIndexNum;
 	}
 	
 	@FXML
