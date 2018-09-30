3f5922723d709b3fc039110a78c0938e63bde267
Rodney McQuain <rodneymcquain95@gmail.com>
2018-09-29 21:16:18 -0400

    Added paymentID field, constructors, and getter/setter
    Added default constructor and constructor for paymentID field and a getter and setter for paymentID.

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Payment {
-
+	private int paymentID;
+	
+	public Payment(int paymentID) {
+		this.paymentID = paymentID;
+	}
+	
+	public Payment() {
+		
+	}
+	
+	public int getPaymentID() {
+		return paymentID;
+	}
+	
+	public void setPaymentID(int paymentID) {
+		this.paymentID = paymentID;
+	}
 }
